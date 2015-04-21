a60ec251239b7b837837b27111db6b460bbf259a
Gocho Mugo <mugo@forfuture.co.ke>
2015-04-21 15:04:10 +0300

    More console logging functions
    More functions have been added to make logging to the console easier and
    cleaner. These functions allow text styles:
    
    * Bold
    * Underline
    
    Also the logging functions are variadic. They can accept numerous number
    of strings to be output to the console.

--- lib/cli.go
+++ lib/cli.go
@@ -3,26 +3,37 @@ package lib
 import (
 	"fmt"
 	"github.com/mitchellh/colorstring"
+	"github.com/ttacon/chalk"
+	"strings"
 )
 
 const padding = "    "
 
-func log(message string, color string) {
-	colorstring.Println(padding + "⇒ [" + color + "]" + message)
+func log(message []string, color string) {
+	colorstring.Println(padding + "⇒ [" + color + "]" + strings.Join(message, ""))
 }
 
-func LogSuccess(message string) {
+func LogSuccess(message ...string) {
 	log(message, "green")
 }
 
-func LogError(message string) {
+func LogError(message ...string) {
 	log(message, "red")
 }
 
-func LogNormal(message string) {
+func LogNormal(message ...string) {
 	log(message, "white")
 }
 
-func LogRaw(message string) {
+func LogRaw(message ...string) {
 	fmt.Println(message)
 }
+
+func TextBold(str string) string {
+  return chalk.Bold.TextStyle(str)
+}
+
+func TextUnderline(str string) string {
+  return chalk.Underline.TextStyle(str)
+}
+
